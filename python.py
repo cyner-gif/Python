9e919718182027c7ddb0ffdd42702ded1e7f1e85
cyner-gif <manishathapa2135@gmail.com>
2026-07-25 20:57:15 +0545

    Create python.py

---
+++ python.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import webbrowser
+
+root = tk.Tk()
+
+def command():
+  webbrowser.open("https://www.youtube.com")
+command()
+
+l1 = tk.Label(root, text="Click The Button")
+l1.pack()
+
+b1 = tk.Button(root, text="Click Me",command=command)
+b1.pack()
+
+root.mainloop()
